241319a8154af6e22a23d9b7f7fbbaa63e272e8e
inggityeira <inggitagranata22@gmail.com>
2024-12-18 10:33:23 +0700

    WEB PIDS

---
+++ Web-PIDS/website/config.py
@@ -0,0 +1,8 @@
+# config.py
+# import os
+
+class Config:
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'  # Ganti dengan username MySQL Anda
+    MYSQL_PASSWORD = ''  # Ganti dengan password MySQL Anda
+    MYSQL_DB = 'management_stasiun'  # Ganti dengan nama database Anda
