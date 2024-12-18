5980fdeca3e651e100871858f9592310fa72d5f5
dashevaivan <iivan1415@gmail.com>
2024-12-18 11:12:51 +0700

    Commit Login

--- Manajemen-Stasiun/routes/web.php
+++ Manajemen-Stasiun/routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PointController;
+use App\Http\Controllers\loginController;
+
+Route::get('login', [loginController::class, 'showLoginForm'])->name('admin.login');
+Route::post('/login', [loginController::class, 'login']);
+Route::post('/logout', [loginController::class, 'logout'])->name('admin.logout');
 
 Route::get('/', [PointController::class, 'index']);
 Route::post('/inputStasiun', [PointController::class, 'store'])->name('inputStasiun');
